7179b039bdcd766cd779904c6c7278f752661fc6
diana-wang <diana-wang@users.noreply.github.com>
2015-05-23 16:41:06 -0700

    added a map to js

--- boredtoday.js
+++ boredtoday.js
@@ -1,10 +1,14 @@
 "use strict";
 
-window.onload = function() {
-	var find = document.getElementByid("find");
-	find.onclick = findLocations;
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
 }
 
-function findLocations() {
-	
-}
+google.maps.event.addDomListener(window, 'load', initialize);
