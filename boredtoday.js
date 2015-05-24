f89bed2c52ce330e09083b5bd66b7d0bf7ab3610
diana-wang <diana-wang@users.noreply.github.com>
2015-05-23 20:07:54 -0700

    added information window

--- boredtoday.js
+++ boredtoday.js
@@ -1,13 +1,37 @@
 "use strict";
+
+var yelpkey = "oIhHCpDAyBL7Ww4bO5qb7g";   //api key for YELP
 var map;
+var longitude = -34.397;
+var latitude = 150.644;
+var myLatLng = null;
+var businessinfo = "";
 
 function initialize() {
+  myLatLng = new google.maps.LatLng(longitude, latitude);
   var mapOptions = {
     zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    center: myLatLng
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+  var contentString = '<div id="content">'+
+    '<h1 id="businessinfo"><?php echo $name; ?></h1>' +
+    '<p>Yelp Id: <?php echo $yelpid; ?></p>';
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: name
+  });
+
+  google.maps.event.addListener(marker, 'click', function() { infowindow.open(map, marker);});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
